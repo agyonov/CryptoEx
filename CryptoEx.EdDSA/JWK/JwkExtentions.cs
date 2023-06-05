ee042574071ad4f3506564a806c95c0fec618941
Aleksandar Gyonov <agyonov@hotmail.com>
2023-06-05 19:33:14 +0300

    Some Jwk Convertions

--- CryptoEx.EdDSA/JWK/JwkExtentions.cs
+++ CryptoEx.EdDSA/JWK/JwkExtentions.cs
@@ -1,4 +1,8 @@
-﻿namespace CryptoEx.Ed.JWK;
+﻿using CryptoEx.EdDSA;
+using CryptoEx.JWK;
+using CryptoEx.Utils;
+
+namespace CryptoEx.Ed.JWK;
 
 /// <summary>
 /// Some extentiom methods for JWK - to get the key from the JWK
@@ -6,78 +10,74 @@
 /// </summary>
 public static class JwkExtentions
 {
-    ///// <summary>
-    ///// Gets the EdDsa public key from the jwk or null if the jwk does not have an EdDsa public key.
-    ///// </summary>
-    //public static ECDsa? GetECDsaPublicKey(this Jwk jwk)
-    //{
-    //    //check the key type
-    //    if (jwk is not JwkEd) {
-    //        return null;
-    //    }
+    /// <summary>
+    /// Gets the EdDsa public key from the jwk or null if the jwk does not have an EdDsa public key.
+    /// </summary>
+    public static EdDsa? GetEdDsaPublicKey(this Jwk jwk)
+    {
+        //check the key type
+        if (jwk is not JwkEd) {
+            return null;
+        }
 
-    //    //get the key
-    //    JwkEd jEd = (JwkEd)jwk;
+        //get the key
+        JwkEd jEd = (JwkEd)jwk;
 
-    //    // check the key
-    //    if (string.IsNullOrEmpty(jEc.X) || string.IsNullOrEmpty(jEc.Y) || string.IsNullOrEmpty(jEc.Crv)) {
-    //        return null;
-    //    }
+        // check the key
+        if (string.IsNullOrEmpty(jEd.X) || string.IsNullOrEmpty(jEd.Crv)) {
+            return null;
+        }
 
-    //    //try create the EC key
-    //    try {
-    //        ECParameters param = new ECParameters();
-    //        param.Q = new ECPoint();
-    //        param.Q.X = Base64UrlEncoder.Decode(jEc.X);
-    //        param.Q.Y = Base64UrlEncoder.Decode(jEc.Y);
-    //        param.Curve = jEc.Crv switch
-    //        {
-    //            JwkConstants.CurveP256 => ECCurve.NamedCurves.nistP256,
-    //            JwkConstants.CurveP384 => ECCurve.NamedCurves.nistP384,
-    //            JwkConstants.CurveP521 => ECCurve.NamedCurves.nistP521,
-    //            _ => throw new Exception($"Unknown curve name {jEc.Crv}")
-    //        };
-    //        return ECDsa.Create(param);
-    //    } catch {
-    //        return null;
-    //    }
-    //}
+        //try create the EC key
+        try {
+            EDParameters param = new();
+            param.X = Base64UrlEncoder.Decode(jEd.X);
+            param.Crv = jEd.Crv switch
+            {
+                JwkConstants.CurveEd25519 => EdConstants.OidEd25519,
+                JwkConstants.CurveEd448 => EdConstants.OidEd448,
+                _ => throw new Exception($"Unknown curve name {jEd.Crv}")
+            };
 
-    ///// <summary>
-    ///// Gets the EdDsa private key from the jwk or null if the jwk does not have an EdDsa private key.
-    ///// </summary>
-    //public static ECDsa? GetECDsaPrivateKey(this Jwk jwk)
-    //{
-    //    //check the key type
-    //    if (jwk is not JwkEc) {
-    //        return null;
-    //    }
+            return EdDsa.Create(param);
+        } catch {
+            return null;
+        }
+    }
 
-    //    //get the key
-    //    JwkEc jEc = (JwkEc)jwk;
+    /// <summary>
+    /// Gets the EdDsa private key from the jwk or null if the jwk does not have an EdDsa private key.
+    /// </summary>
+    public static EdDsa? GetEdDsaPrivateKey(this Jwk jwk)
+    {
+        //check the key type
+        if (jwk is not JwkEd) {
+            return null;
+        }
 
-    //    // check the key
-    //    if (string.IsNullOrEmpty(jEc.X) || string.IsNullOrEmpty(jEc.Y) || string.IsNullOrEmpty(jEc.Crv) || string.IsNullOrEmpty(jEc.D)) {
-    //        return null;
-    //    }
+        //get the key
+        JwkEd jEd = (JwkEd)jwk;
 
-    //    //try create the EC key
-    //    try {
-    //        ECParameters param = new ECParameters();
-    //        param.Q = new ECPoint();
-    //        param.Q.X = Base64UrlEncoder.Decode(jEc.X);
-    //        param.Q.Y = Base64UrlEncoder.Decode(jEc.Y);
-    //        param.D = Base64UrlEncoder.Decode(jEc.D);
-    //        param.Curve = jEc.Crv switch
-    //        {
-    //            JwkConstants.CurveP256 => ECCurve.NamedCurves.nistP256,
-    //            JwkConstants.CurveP384 => ECCurve.NamedCurves.nistP384,
-    //            JwkConstants.CurveP521 => ECCurve.NamedCurves.nistP521,
-    //            _ => throw new Exception($"Unknown curve name {jEc.Crv}")
-    //        };
-    //        return ECDsa.Create(param);
-    //    } catch {
-    //        return null;
-    //    }
-    //}
+        // check the key
+        if (string.IsNullOrEmpty(jEd.X) || string.IsNullOrEmpty(jEd.Crv) || string.IsNullOrEmpty(jEd.D)) {
+            return null;
+        }
+
+        //try create the EC key
+        try {
+            EDParameters param = new();
+            param.X = Base64UrlEncoder.Decode(jEd.X);
+            param.D = Base64UrlEncoder.Decode(jEd.D);
+            param.Crv = jEd.Crv switch
+            {
+                JwkConstants.CurveEd25519 => EdConstants.OidEd25519,
+                JwkConstants.CurveEd448 => EdConstants.OidEd448,
+                _ => throw new Exception($"Unknown curve name {jEd.Crv}")
+            };
+
+            return EdDsa.Create(param);
+        } catch {
+            return null;
+        }
+    }
 }

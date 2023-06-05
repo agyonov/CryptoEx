6e37d16093a492f16160acdc42cf714d1355848e
Aleksandar Gyonov <agyonov@hotmail.com>
2023-06-05 14:20:09 +0300

    Some

--- CryptoEx.EdDSA/EdDsa/EdDsa.Sign.cs
+++ CryptoEx.EdDSA/EdDsa/EdDsa.Sign.cs
@@ -1,5 +1,6 @@
 ﻿using CryptoEx.Ed;
 using Org.BouncyCastle.Crypto.Parameters;
+using System.Security.Cryptography;
 
 namespace CryptoEx.EdDSA;
 
@@ -9,7 +10,38 @@ namespace CryptoEx.EdDSA;
 public partial class EdDsa : EDAlgorithm
 {
 
+    /// <summary>
+    /// Sign data
+    /// </summary>
+    public byte[] Sign(ReadOnlySpan<byte> data)
+    {
+        // Check if we have a signer
+        if (_Signer == null) {
+            return Array.Empty<byte>();
+        }
 
+        // What is the curve
+        switch (_EdAlgorithm) {
+            case EdAlgorithm.Ed25519:
+                // Check signer
+                if (_PrivateKey25519 == null) { 
+                    throw new CryptographicException("No private key");
+                } 
+                _Signer.Init(true, _PrivateKey25519);
+                break;
+            case EdAlgorithm.Ed448:
+                // Check signer
+                if (_PrivateKey448 == null) {
+                    throw new CryptographicException("No private key");
+                } 
+                _Signer.Init(true, _PrivateKey448);
+                break;
+        }
+
+        // Sign
+        _Signer.BlockUpdate(data);
+        return _Signer.GenerateSignature();
+    }
 
 
 }
--- CryptoEx.EdDSA/EdDsa/EdDsa.Verify.cs
+++ CryptoEx.EdDSA/EdDsa/EdDsa.Verify.cs
@@ -8,5 +8,30 @@ namespace CryptoEx.EdDSA;
 /// </summary>
 public partial class EdDsa : EDAlgorithm
 {
-    
+    /// <summary>
+    /// Verify signature
+    /// </summary>
+    public bool Verify(ReadOnlySpan<byte> data, byte[] signature)
+    {
+        // Check if we have a signer
+        if (_Signer == null) {
+            return false;
+        }
+
+        // What is the curve
+        switch (_EdAlgorithm) {
+            case EdAlgorithm.Ed25519:
+                // Init signer
+                _Signer.Init(false, _PublicKey25519);
+                break;
+            case EdAlgorithm.Ed448:
+                // Init signer
+                _Signer.Init(false, _PublicKey448);
+                break;
+        }
+
+        // Sign
+        _Signer.BlockUpdate(data);
+        return _Signer.VerifySignature(signature);
+    }
 }

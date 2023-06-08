db124b921590b17755607c40cd98e9fb25ba177f
Aleksandar Gyonov <agyonov@hotmail.com>
2023-06-08 13:00:43 +0300

    Add some pfxs to the benchmark projected

--- CryptoEx.Benchmark/EtsiXml/EcdsSignVerify.cs
+++ CryptoEx.Benchmark/EtsiXml/EcdsSignVerify.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using CryptoEx.Ed.EdDsa;
 using CryptoEx.XML.ETSI;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
@@ -145,44 +146,63 @@ public class EcdsSignVerify
     // Get some certificate from the store for testing
     private static X509Certificate2? GetCertificate(CertType certType)
     {
-        var now = DateTime.Now;
-        using (X509Store store = new X509Store(StoreLocation.CurrentUser)) {
-            store.Open(OpenFlags.ReadOnly);
+        //var now = DateTime.Now;
+        //using (X509Store store = new X509Store(StoreLocation.CurrentUser)) {
+        //    store.Open(OpenFlags.ReadOnly);
 
-            var coll = store.Certificates
-                            .Where(cert => cert.HasPrivateKey && cert.NotBefore < now && cert.NotAfter > now)
-                            .ToList();
+        //    var coll = store.Certificates
+        //                    .Where(cert => cert.HasPrivateKey && cert.NotBefore < now && cert.NotAfter > now)
+        //                    .ToList();
 
-            List<X509Certificate2> valColl = new List<X509Certificate2>();
+        //    List<X509Certificate2> valColl = new List<X509Certificate2>();
 
-            foreach (var c in coll) {
-                using (var chain = new X509Chain()) {
+        //    foreach (var c in coll) {
+        //        using (var chain = new X509Chain()) {
 
-                    chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
-                    chain.ChainPolicy.DisableCertificateDownloads = true;
-                    if (chain.Build(c)) {
-                        valColl.Add(c);
+        //            chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+        //            chain.ChainPolicy.DisableCertificateDownloads = true;
+        //            if (chain.Build(c)) {
+        //                valColl.Add(c);
+        //            } else {
+        //                c.Dispose();
+        //            }
+
+        //            for (int i = 0; i < chain.ChainElements.Count; i++) {
+        //                chain.ChainElements[i].Certificate.Dispose();
+        //            }
+        //        }
+        //    }
+
+        //    return valColl.Where(c =>
+        //    {
+        //        string frName = certType switch
+        //        {
+        //            CertType.RSA => "RSA",
+        //            CertType.EC => "ECC",
+        //            _ => "Ed"
+        //        };
+        //        return c.PublicKey.Oid.FriendlyName == frName;
+        //    })
+        //    .FirstOrDefault();
+        //}
+
+        // Check what we need
+        switch (certType) {
+            case CertType.RSA:
+                return new X509Certificate2(@"source\cerRSA.pfx", "pass.123");
+            case CertType.EC:
+                return new X509Certificate2(@"source\cerECC.pfx", "pass.123");
+            case CertType.Ed:
+                using (FileStream fs = new(@"source\cert.pfx", FileMode.Open, FileAccess.Read)) {
+                    X509Certificate2Ed[] arrCerts = fs.LoadEdCertificatesFromPfx("pass.123");
+                    if (arrCerts.Length > 0) {
+                        return arrCerts[0].Certificate;
                     } else {
-                        c.Dispose();
-                    }
-
-                    for (int i = 0; i < chain.ChainElements.Count; i++) {
-                        chain.ChainElements[i].Certificate.Dispose();
+                        return null;
                     }
                 }
-            }
-
-            return valColl.Where(c =>
-            {
-                string frName = certType switch
-                {
-                    CertType.RSA => "RSA",
-                    CertType.EC => "ECC",
-                    _ => "Ed"
-                };
-                return c.PublicKey.Oid.FriendlyName == frName;
-            })
-            .FirstOrDefault();
+            default:
+                return null;
         }
     }
 }
